693d2056003f88cb4f02a7c23e36a77680f709c8
Guudz <Kilian1702@outlook.com>
2023-02-13 16:26:01 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_arcanethenestv.meta',
+  'mp_f_freemode_01_mp_f_arcanethenestv.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_arcanethenestv.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_arcanethenestv.meta'
